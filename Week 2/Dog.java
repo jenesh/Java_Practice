304193c1460ab45544b38976fa970df22cf0d045
Jenesh <jenesh.napit@gmail.com>
2019-07-14 21:59:26 -0400

    Week 2

--- Week 2/Dog.java
+++ Week 2/Dog.java
@@ -1,5 +1,12 @@
 public class Dog {
-    public static void makenoise () {
-        System.out.print("Bark");
+    public int weightInPounds;
+    public void makenoise () {
+        if (weightInPounds < 10) {
+            System.out.println("Meow! Meow!");
+        } else if (weightInPounds < 30) {
+            System.out.println("Bark! Bark!");
+        } else {
+            System.out.println("Wolf!");
+        }
     }
 }
